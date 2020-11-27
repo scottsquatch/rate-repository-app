6d7719bd456248ebadece1f1c663d57ab36d81fb
Scott Dose <sdose27@gmail.com>
2020-11-27 10:57:06 -0800

    Complete exercise 10.5

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,17 +1,93 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    flex: 1,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    padding: 10,
+  },
+  language: {
+    backgroundColor: theme.colors.primary,
+    color: 'white',
+    padding: 5,
+  },
+  topContainer: {
+    flexDirection: 'row',
+    padding: 5,
+    flex: 1,
+  },
+  infoContainer: {
+    paddingLeft: 20,
+    paddingBottom: 5,
+    paddingRight: 5,
+    justifyContent: 'space-between',
+    flex: 1,
+  },
+  infoItem: {
+    paddingBottom: 5,
+    flexWrap: 'wrap',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  statContainer: {
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    justifyContent: 'space-around',
+    alignContent: 'center',
+  },
+  statText: {
+    alignSelf: 'center',
+  }
+});
 
 const RepositoryItem = ({ item }) => {
   return (
-  <View>
-    <Text>Full name: {item.fullName}</Text>
-    <Text>Description: {item.description}</Text>
-    <Text>Language: {item.language}</Text>
-    <Text>Stars: {item.stargazersCount}</Text>
-    <Text>Forks: {item.forksCount}</Text>
-    <Text>Reviews: {item.reviewCount}</Text>
-    <Text>Rating: {item.ratingAverage}</Text>
+  <View style={styles.container}>
+    <View style={styles.topContainer}>
+      <Image 
+        style={styles.avatar}
+        source={{ uri: item.ownerAvatarUrl }}
+      />
+      <View style={styles.infoContainer}>
+        <View style={styles.infoItem}>
+          <Text fontWeight="bold">{item.fullName}</Text>
+        </View>
+        <View sylte={styles.infoItem}>
+          <Text color="textSecondary">{item.description}</Text>
+        </View>
+        <View style={styles.infoItem}>
+          <Text style={styles.language}>{item.language}</Text>
+        </View>
+      </View>
+    </View>
+    <View style={styles.statContainer}>
+      <View>
+        <Text fontWeight="bold" style={styles.statText}>{item.stargazersCount > 1000 ? (item.stargazersCount / 1000).toFixed(1) + ' k' : item.stargazersCount}</Text>
+        <Text color="textSecondary" style={styles.statText}>Stars</Text>
+      </View>
+      <View>
+        <Text fontWeight="bold" style={styles.statText}>{item.forksCount > 1000 ? (item.forksCount / 1000).toFixed(1) + 'k' : item.forksCount}</Text>
+        <Text color="textSecondary" style={styles.statText}>Forks</Text>
+      </View>
+      <View>
+        <Text fontWeight="bold" style={styles.statText}>{item.reviewCount}</Text>
+        <Text color="textSecondary" style={styles.statText}>Reviews</Text>
+      </View>
+      <View>
+        <Text fontWeight="bold" style={styles.statText}>{item.ratingAverage}</Text>
+        <Text color="textSecondary" style={styles.statText}>Rating</Text>
+      </View>
+    </View>
   </View>);
 }
 
